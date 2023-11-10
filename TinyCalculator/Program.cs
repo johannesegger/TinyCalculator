37c6a4053993b9e14080a841eb4770bac0d8ceb3
Johannes Egger <eggj@htlvb.at>
2023-11-10 08:25:04 +0100

    Continue calculation and use result as next dividend

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,19 +1,23 @@
 ﻿Console.WriteLine("== Tiny calculator ==");
 
-int dividend = ReadNumber("Dividend");
-int divisor = ReadNumber("Divisor");
+int result = ReadNumber("Dividend");
+while (true)
+{
+    int divisor = ReadNumber("Divisor");
 
-if (divisor == 0)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Divisor darf nicht 0 sein.");
-    Console.ResetColor();
-}
-else
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
-    Console.ResetColor();
+    if (divisor == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Divisor darf nicht 0 sein.");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{result} / {divisor} = {result / divisor}");
+        result = result / divisor;
+        Console.ResetColor();
+    }
 }
 
 static int ReadNumber(string name)
