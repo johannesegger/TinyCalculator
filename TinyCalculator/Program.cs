56b56eaf3d4e182e5b45b0b9b3f05c85f52c5853
Johannes Egger <eggj@htlvb.at>
2023-11-10 08:43:35 +0100

    Choose between four basic operators

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,18 +1,19 @@
 ﻿Console.WriteLine("== Tiny calculator ==");
 
-int dividend = ReadNumber("Dividend");
-int divisor = ReadNumber("Divisor");
+ICalculation operation = ChooseOperator();
 
-if (divisor == 0)
+int leftOperand = ReadNumber(operation.LeftOperandName);
+int rightOperand = ReadNumber(operation.RightOperandName);
+if (operation.Calculate(leftOperand, rightOperand, out string? errorMessage) is int result)
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Divisor darf nicht 0 sein.");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{leftOperand} {operation.Operator} {rightOperand} = {result}");
     Console.ResetColor();
 }
 else
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(errorMessage);
     Console.ResetColor();
 }
 
@@ -29,5 +30,83 @@ static int ReadNumber(string name)
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"{input} is not a number.");
         Console.ResetColor();
+    }
+}
+
+ICalculation ChooseOperator()
+{
+    while (true)
+    {
+        Console.Write($"Operator [+ - * /]: ");
+        string input = Console.ReadLine();
+        if (input == "+") return new Addition();
+        else if (input == "-") return new Subtraction();
+        else if (input == "*") return new Multiplication();
+        else if (input == "/") return new Division();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{input} is not a valid operator.");
+        Console.ResetColor();
+    }
+}
+
+interface ICalculation
+{
+    string LeftOperandName { get; }
+    string Operator { get; }
+    string RightOperandName { get; }
+    int? Calculate(int a, int b, out string? errorMessage);
+}
+
+class Addition : ICalculation
+{
+    public string LeftOperandName => "Summand 1";
+    public string Operator => "+";
+    public string RightOperandName => "Summand 2";
+    public int? Calculate(int a, int b, out string? errorMessage)
+    {
+        errorMessage = null;
+        return a + b;
+    }
+}
+
+class Subtraction : ICalculation
+{
+    public string LeftOperandName => "Minuend";
+    public string RightOperandName => "Subtrahend";
+    public string Operator => "-";
+    public int? Calculate(int a, int b, out string? errorMessage)
+    {
+        errorMessage = null;
+        return a - b;
+    }
+}
+
+class Division : ICalculation
+{
+    public string LeftOperandName => "Dividend";
+    public string RightOperandName => "Divisor";
+    public string Operator => "/";
+    public int? Calculate(int a, int b, out string? errorMessage)
+    {
+        if (b == 0)
+        {
+            errorMessage = "Divisor darf nicht 0 sein.";
+            return null;
+        }
+        errorMessage = null;
+        return a / b;
+    }
+}
+
+class Multiplication : ICalculation
+{
+    public string LeftOperandName => "Factor 1";
+    public string RightOperandName => "Factor 2";
+    public string Operator => "*";
+    public int? Calculate(int a, int b, out string? errorMessage)
+    {
+        errorMessage = null;
+        return a * b;
     }
 }
